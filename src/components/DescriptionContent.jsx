965791f3f0df01aa538c040d9e106639114ae899
Mannan Dassani <mannandassani1@gmail.com>
2023-03-29 12:02:01 +0530

    Changed the final scrollbar

--- src/components/DescriptionContent.jsx
+++ src/components/DescriptionContent.jsx
@@ -16,7 +16,7 @@ export default function DescriptionContent(){
 
     return(
         <div className="relative pt-1 flex flex-col">
-            <div className="h-[84px] p-0.5 px-0 border border-gray-300 rounded-lg  focus:outline-none">
+            <div className="h-[84px] p-1 pr-0 border border-gray-300 rounded-lg  focus:outline-none">
                 <div 
                 className="p-2 h-full pr-8 break-all cursor-text text-sm font-normal overflow-y-auto oslashSnippetEditor-scroll-bar text-gray-900 focus:outline-none" 
                 onInput={() => handleChange()}
